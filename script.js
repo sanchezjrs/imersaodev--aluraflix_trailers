29143b6d14839ea2e00d053bc62694b8998353d5
Antonio Sanchez <82811454+sanchezjrs@users.noreply.github.com>
2021-04-19 23:49:10 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function adicionarFilme() {
+    var campoFilmeFavorito = document.querySelector('#filme')
+    var filmeFavorito = campoFilmeFavorito.value
+    if (filmeFavorito.endsWith('.jpg')){
+        listarFilmesNaTela(filmeFavorito)
+      }  else{
+        alert("Imagem inválida")
+      }
+      campoFilmeFavorito.value = ""
+    }
+  function listarFilmesNaTela(filme) {
+    var listaFilmes = document.querySelector('#listaFilmes')
+    var elementoFilme = "<img src=" + filme + ">" 
+    listaFilmes.innerHTML = listaFilmes.innerHTML + elementoFilme
+  }
